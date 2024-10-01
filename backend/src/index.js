ff86af8165b80bcddca2c85d5c7c919257708873
Marcoxmas <marco.nat2001@gmail.com>
2024-10-01 18:37:45 +0200

    fixed routing issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+//TODO: INITIAL SETUP OF THE DATABASE
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('Error connecting to the database:', err);
@@ -15,6 +16,26 @@ pool.query('SELECT NOW()', (err, res) => {
     console.log('Connected to the database at:', res.rows[0].now);
   }
 });
+
+// create users table
+pool.query(
+  `CREATE TABLE IF NOT EXISTS "User" (
+            ID SERIAL PRIMARY KEY,
+            NAME VARCHAR(100) NOT NULL,
+            SURNAME VARCHAR(100) NOT NULL,
+            EMAIL VARCHAR(255) UNIQUE NOT NULL,
+            HASH_PASS VARCHAR(255) NOT NULL,
+            WALLET VARCHAR(255),
+            CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`,
+  (err, res) => {
+    if (err) {
+      console.error('Error creating the users table:', err);
+    } else {
+      console.log('Users table created successfully');
+    }
+  }
+);
 
 app.use(cors({
     origin: 'http://localhost:3000'
